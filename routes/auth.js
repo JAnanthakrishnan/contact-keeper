679b44e853e084a4614f79389d24c5fdfe794908
J Ananthakrishnan <jananthakrishnan@pop-os.localdomain>
2020-04-29 21:25:46 +0530

    Starting frontend

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const {check,validationResult} = require('express-validator');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+const bcrypt = require('bcryptjs')
+const auth = require('../middleware/auth')
 
 //  @route        GET api/auth
-//  @desc         Register a user
+//  @desc         Get logged in user
 //  @access       Private
-router.get('/',(req,res)=>{
-    res.send('Get logged in user');
+router.get('/',auth,async (req,res)=>{
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 
@@ -14,8 +25,40 @@ router.get('/',(req,res)=>{
 //  @route      POST api/auth
 //  @desc       Auth user and get token
 //  @access     Public
-router.post('/',(req,res)=>{
-    res.send('Log in user');
+router.post('/',[
+    check('email','Please include a valid email').isEmail(),
+    check('password','Password is required').exists()
+],
+async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg:'Invalid credentials'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({msg:'Wrong Password'});
+        }
+        const payload = {
+            user:{
+                id:user.id
+            }
+        };
+        jwt.sign(payload,config.get('jwtSecret'),{
+            expiresIn:360000
+        },(err,token)=>{
+            if (err) throw err;
+            res.json({token});
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
 });
 
 
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+//Connect Database
+connectDB();
+
+//init body parser middleware
+app.use(express.json({extended:false}));
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>res.json({msg:'Welcome to the contact keeper API'}));
 
@@ -7,6 +15,7 @@ app.get('/',(req,res)=>res.json({msg:'Welcome to the contact keeper API'}));
 app.use('/api/users',require('./routes/users'))
 app.use('/api/contacts',require('./routes/contacts'))
 app.use('/api/auth',require('./routes/auth'))
+
 
 
 const PORT = process.env.PORT || 5000;
