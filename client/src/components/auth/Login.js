939bc6486aec711eed82e7f802ca94b5e4f1698f
J Ananthakrishnan <jananthakrishnan@pop-os.localdomain>
2020-05-01 21:51:39 +0530

    Final commits

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -16,7 +16,7 @@ const Login = (props) => {
             setAlert('Email is invalid','danger');
             clearErrors();
         }
-        else if(error!=='No token - authorization denied'){
+        else if(error && error!=='No token - authorization denied'){
             setAlert(error,'danger');
             clearErrors();
         }
