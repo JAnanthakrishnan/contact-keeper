ddee01b4df802e7d8564abbbc153e44ecaa11b6c
J Ananthakrishnan <jananthakrishnan@pop-os.localdomain>
2020-05-01 13:46:52 +0530

    Starting to connect backend

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { useContext, Fragment } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem'
+import {CSSTransition,TransitionGroup} from 'react-transition-group'
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
@@ -10,13 +11,21 @@ const Contacts = () => {
     }
     return (
         <Fragment>
-            {(filtered!==null)
-            ?
-            (filtered.map(contact=>
-            <ContactItem contact={contact} key={contact.id}/>))
-            :
-            (contacts.map(contact=>
-            <ContactItem contact={contact} key={contact.id}/>))}
+            <TransitionGroup>
+                {(filtered!==null)
+                ?
+                (filtered.map(contact=>
+                    <CSSTransition key={contact.id} timeout={500} classNames="item">
+                        <ContactItem contact={contact} />
+                    </CSSTransition>
+                ))
+                :
+                (contacts.map(contact=>
+                    <CSSTransition key={contact.id} timeout={500} classNames="item">
+                        <ContactItem contact={contact} />
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </Fragment>
     )
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,12 @@ export const Navbar = ({title,icon}) => {
                     <Link to='/'>Home</Link>
                 </li>
                 <li>
+                    <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                    <Link to='/login'>Login</Link>
+                </li>
+                <li>
                     <Link to='/about'>About</Link>
                 </li>
             </ul>
